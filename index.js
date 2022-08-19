101847617c3ef321b26f298da7cab8e15a81cfe2
abhannan123 <abdul.hannan@orbiqon.com>
2022-08-19 13:59:07 +0500

    complete the project

--- index.js
+++ index.js
@@ -94,9 +94,10 @@
 // console.log("here is the ", descripter);
 
 //<========HERE IS THE CODE================>
+
 function populate() {
   if (quiz.isEnded()) {
-    //showScores();
+    showScores();
   } else {
     //showQuestions()
     var element = document.getElementById("question");
@@ -106,8 +107,29 @@ function populate() {
     for (var i = 0; i < choices.length; i++) {
       var element = document.getElementById("choice" + i);
       element.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
     }
+    showProgress();
   }
+}
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function () {
+    quiz.guess(guess);
+    populate();
+  };
+}
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML =
+    "question" + currentQuestionNumber + "of" + quiz.questions.length;
+}
+function showScores() {
+  var gameOverHtml = "<h1>Result</h1>";
+  gameOverHtml += "<h2 id='score'>Your Score" + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHtml;
 }
 
 var questions = [
--- quizController.js
+++ quizController.js
@@ -7,11 +7,12 @@ Quiz.prototype.getQuestionIndex = function () {
   return this.questions[this.questionIndex];
 };
 Quiz.prototype.isEnded = function () {
-  return this.questions.length === this.questionIndex;
+  return this.questions.length <= this.questionIndex;
 };
+
 Quiz.prototype.guess = function (answer) {
   this.questionIndex++;
-  if ((this.getQuestionIndex(), correctAnswer(answer))) {
+  if (this.getQuestionIndex().correctAnswer(answer)) {
     this.score++;
   }
 };
